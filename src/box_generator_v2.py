d69ae5259a64f0fc10981c7a4683f3f300edccc3
simple sketch <71196100+simpleSketche@users.noreply.github.com>
2022-11-05 18:46:13 -0400

    create v2 box_generator, create_edge, create_node

---
+++ src/box_generator_v2.py
@@ -0,0 +1,96 @@
+import rhinoinside
+rhinoinside.load()
+import create_node_json
+import create_edge_json
+import math
+import box
+import generator
+import rule
+import Rhino.Geometry as rg
+import option
+import random
+
+
+
+
+def generate(num_option):
+    # loop 5000 data points
+    for i in range(0, num_option):
+
+        # create rules
+        num_sels = 10
+        option = i
+        cur_rule = rule.Rule(num_sels, option)
+
+        # get the rules
+        rules = cur_rule.get_rules()
+        rule_seq = cur_rule.get_rule_seq()
+
+        # create dataset
+        brick = box.Brick
+        cur_generator = generator.Stack_bricks
+        # brick parametric data
+        brick_red = {"depth": 6, "width": 8, "type": "A1"}
+        brick_green = {"depth": 15, "width": 8, "type": "B1"}
+        brick_blue = {"depth": 6, "width": 16, "type": "C1"}
+        brick_data = [brick_red, brick_green, brick_blue]
+        target_n = math.floor(option / 1000) + 4
+        start_plane = rg.Plane.WorldXY
+        stack_bricks = cur_generator(
+            brick_data, rules, target_n, start_plane, option, False, rule_seq)
+        stack_bricks.generate()
+
+        # All the stuff below are for visualization
+        planes = [brick.origin for brick in stack_bricks.bricks]
+        footprints = [brick.footprint for brick in stack_bricks.bricks]
+        used_ports = stack_bricks.connected_ports
+        used_bricks = stack_bricks.connected_bricks
+        connected_ports_name = stack_bricks.connected_ports_name
+        grow_rule_seq = stack_bricks.grow_rule_seq
+
+        bricks = stack_bricks.bricks
+
+        # create correct edges
+        edge_json = create_edge_json.Edge_json(
+            bricks, 12, 12, rg.Point3d(0, 0, 0), option)
+        edge_json.parse_path()
+        edge_json.save_json_data()
+        
+        is_bad_data = test_bad_data(bricks.intersection)
+
+        if(is_bad_data == False):
+            generate_error_data(bricks)
+            node_json = create_node_json.Node_json(
+                bricks, 12, 12, rg.Point3d(0, 0, 0), option)
+            node_json.parse_path()
+            node_json.save_json_data()
+            print("------------------------------------------")
+            print("currently creating data point {}".format(i))
+            print("Finished creating data point {}".format(i))
+            print("------------------------------------------")
+
+def generate_error_data(bricks):
+    for brick in bricks:
+        footprint = brick.footprint.ToNurbsCurve()
+        cnt = rg.AreaMassProperties.Compute(footprint).Centroid
+        cur_brick_loc = cnt
+        trans = create_random_loc(cur_brick_loc)
+        brick.move(trans)
+
+def create_random_loc(originPt):
+    randomX = round(random.random(), 5)
+    randomY = round(random.random(), 5)
+    newVec = rg.Vector3d(randomX*0.5, randomY*0.5, 0)
+    trans = rg.Transform.Translation(newVec)
+    return trans
+
+def test_bad_data(intersections):
+    is_bad = False
+    for intersection in intersections:
+        if(intersection == True):
+            is_bad = True
+            return is_bad
+
+
+if __name__ == "__main__":
+    generate(option.option)
---
+++ src/create_edge_json_v2.py
@@ -0,0 +1,61 @@
+
+
+import rhinoinside
+import Rhino
+import Rhino.Geometry as rg
+import math
+import json
+import os
+
+
+class Edge_json():
+    def __init__(self, boxes, siteWidth, siteLength, siteLoc, option):
+        self.boxes = boxes
+        self.siteWidth = siteWidth
+        self.siteLength = siteLength
+        self.siteLoc = siteLoc
+        self.option = option
+    
+    def get_cur_path(self):
+        cur_path = os.getcwd().split('\\')
+        cur_path = cur_path[:-1]
+        return cur_path
+
+    def parse_path(self):
+        root_path = self.get_cur_path()
+        root_path.append('data\\edges')
+        new_path = '\\'.join(root_path)
+        is_path_exist = os.path.exists(new_path)
+        if(is_path_exist==False):
+            os.makedirs(new_path)
+        return new_path
+    
+    def parse_json_path(self):
+        json_path = self.get_cur_path()
+        json_path.append('data\\edges\\{}.json'.format(int(self.option)))
+        new_json_path = '\\'.join(json_path)
+        return new_json_path
+
+    def is_edge(self, cur_node, other_node):
+        isEdge = False
+        intersection = rg.Intersect.Intersection.CurveCurve(cur_node.footprint.ToNurbsCurve(), other_node.footprint.ToNurbsCurve(), 0.00000001, 0.00000001)
+        if(len(intersection) == 1):
+            isEdge = True
+        return isEdge
+
+    def create_json_data(self):
+        output_edges_data = {"adjacency": []}
+        for box in self.boxes:
+            for otherbox in self.boxes:
+                if(box.id != otherbox.id):
+                    isCreateEdge = self.is_edge(box, otherbox)
+                    if(isCreateEdge):
+                        cur_edge = [box.id, otherbox.id]
+                        output_edges_data["adjacency"].append(cur_edge)
+        return output_edges_data
+    
+    def save_json_data(self):
+        cur_path = self.parse_json_path()
+        edge_data = self.create_json_data()
+        with open(cur_path, 'w') as f:
+            f.write(json.dumps(edge_data))
